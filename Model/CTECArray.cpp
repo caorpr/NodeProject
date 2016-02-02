e397f2eaa365acda92f68cbcf4923c40a1653d5d
caorpr <caorpr@gmail.com>
2016-02-02 13:30:05 -0700

    Traversing the array to retrieve a value
    First though, you check to see if it is in-bounds

--- Model/CTECArray.cpp
+++ Model/CTECArray.cpp
@@ -6,17 +6,58 @@
  */
 
 #include "CTECArray.h"
+using namespace std;
 
 template <class Type>
-CTECArray::CTECArray()
+CTECArray<Type>::CTECArray(int size)
 {
 	// TODO Auto-generated constructor stub
 	this->size = size;
 }
 
-CTECArray::~CTECArray()
+template <class Type>
+CTECArray<Type>::~CTECArray()
 {
 	// TODO Auto-generated destructor stub
 
 }
 
+template <class Type>
+int CTECArray<Type> :: getSize()
+{
+	return this->size;
+}
+
+template<class Type>
+Type* CTECArray<Type> :: get(int position)
+{
+	//bounds checking needed to not crash!
+	if(position >= size || position < 0)
+	{
+			// Out of bounds! OB!
+		cerr << "position is OB (out of bounds)!" << endl;
+		return nullptr;
+	}
+	else
+	{
+			//In the bounds!!
+		ArrayNode<Type> * current = head;
+		for(int spot = 0; spot <= position; spot++)
+		{
+			if(spot != position)
+			{
+				current = current->getNext();
+			}
+			else
+			{
+				return current->getValue();
+			}
+		}
+	}
+}
+
+template <class Type>
+void CTECArray<Type> :: set(int position, Type value)
+{
+
+}
