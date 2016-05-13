bd45575ee1f200ebffd7e3cc315f04e32cd4f1c1
caorpr <caorpr@gmail.com>
2016-05-13 12:28:34 -0600

    debug?

---
+++ Debug/Controller/NodeRunner.d
@@ -0,0 +1,27 @@
+Controller/NodeRunner.o: ../Controller/NodeRunner.cpp \
+  ../Controller/NodeController.h ../Controller/../Model/Node.cpp \
+  ../Controller/../Model/Node.h ../Controller/../Model/ArrayNode.cpp \
+  ../Controller/../Model/ArrayNode.h \
+  ../Controller/../Model/CTECArray.cpp \
+  ../Controller/../Model/CTECArray.h ../Controller/../Model/CTECList.cpp \
+  ../Controller/../Model/CtecList.h ../Controller/../Model/Timer.h
+
+../Controller/NodeController.h:
+
+../Controller/../Model/Node.cpp:
+
+../Controller/../Model/Node.h:
+
+../Controller/../Model/ArrayNode.cpp:
+
+../Controller/../Model/ArrayNode.h:
+
+../Controller/../Model/CTECArray.cpp:
+
+../Controller/../Model/CTECArray.h:
+
+../Controller/../Model/CTECList.cpp:
+
+../Controller/../Model/CtecList.h:
+
+../Controller/../Model/Timer.h:
